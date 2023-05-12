0b594f995c58a52cc0045f606b5ac796041fff23
Nadezda Samorokova <nadezdasamorokova@gmail.com>
2023-05-12 17:39:22 +0200

    added PetList

--- src/pages/PetList.js
+++ src/pages/PetList.js
@@ -1,8 +1,22 @@
 import Corgi from "../images/PetList/Corgi.png"
 import Cat from "../images/PetList/Cat.png"
 import Mouse from "../images/PetList/Mouse.png"
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function PetList () {
+  const [pets, setPets] = useState([])
+
+  console.log(pets)
+
+  useEffect(() => {
+    axios.get('http://localhost:5005/pets/pets')
+    .then(response => {
+      console.log(response.data.petsFromDB[0].image)
+      setPets(response.data.petsFromDB)
+    })
+  }, [])
+
     return (
         <div className="PetList">
           <div className="petList-sort">
@@ -22,12 +36,14 @@ function PetList () {
           </div>
 
           <div className="pets">
-            <div>
-                <img/>
-                <p></p>
-            </div>
+            {pets.map((pet) => (
+              <div key={pet._id} className="pet"> 
+                <img className="petImage" src={pet.image} alt ="petsPicture"/>
+                <p className="petName">{pet.name}</p>
+              </div>
+            ))}
           </div>
-        </div>
+      </div>
     )
 }
 
